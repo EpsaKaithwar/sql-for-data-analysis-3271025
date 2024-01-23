192f48cf891bd374eab8ed646db590482989d29f
EpsaKaithwar <157450725+EpsaKaithwar@users.noreply.github.com>
2024-01-23 15:17:48 +0000

    Created a TotalDue query

---
+++ TotalDue.sql
@@ -0,0 +1,10 @@
+SELECT Customer.CustomerID,
+  FirstName,
+  LastName,
+  Sum(TotalDue)
+FROM Customer
+  JOIN Orders on Customer.CustomerID = Orders.CustomerID
+GROUP BY Customer.CustomerID,
+  FirstName,
+  LastName
+ORDER BY Sum (TotalDue)
